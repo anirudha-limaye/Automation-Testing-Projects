6a0c7d12694598ea01786e2f8be7e02c0f77f3ee
anirudha-limaye <anirudha.limaye26@gmail.com>
2024-12-30 14:51:52 +0530

    Written locators and methods for Login Page

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,1 +1,29 @@
+package pages;
+import org.openqa.By;
+import org.openqa.WebDriver;
 
+public class LoginPage
+{
+  WebDriver driver;
+
+  
+  By email = By.id("email");
+  By password = By.id("pass");
+  By loginButton = By.id("send2");
+
+  public LoginPage(WebDriver driver)
+  {
+    this.driver = driver;
+  }
+  public void setEmail(String userEmail)
+  {
+    driver.findElement(email).sendKeys(userEmail);
+  }
+  public void setPassword(String pwd)
+  {
+    driver.findElement(password).sendKeys(pwd);
+  }
+  public void clickLogin()
+  {
+    driver.findElement(loginButton).click();
+  }
